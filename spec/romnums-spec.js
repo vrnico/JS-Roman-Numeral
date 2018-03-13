059aa22b37a76615fc15859f738ed30a62e19ce8
Nico Daunt and Roger Yee <roger@>
2018-03-13 15:24:38 -0700

    create green test condition for 5 to V test method

--- spec/romnums-spec.js
+++ spec/romnums-spec.js
@@ -6,4 +6,9 @@ describe('Romnum', function() {
     var isInteger = new Romnum(4);
     expect(isInteger.checkType()).toEqual("this is an integer");
   });
+
+  it('tests if the inputted number is a 5 and if it is a 5 it returns a V', function() {
+    var isInteger = new Romnum(5);
+    expect(isInteger.checkV()).toEqual("V");
+  });
 });
--- src/romnum.js
+++ src/romnum.js
@@ -6,4 +6,10 @@ Romnum.prototype.checkType = function() {
   if ((this.numInput > 0)) {
     return "this is an integer";
   }
+}
+
+Romnum.prototype.checkV = function() {
+  if ((this.numInput === 5)) {
+    return "V";
+  }
 };
