10f21991aa4e19e4b10bfc955ef15d98f28801f2
Nico Daunt and Roger Yee <nico+roger@>
2018-03-13 16:50:50 -0700

    adds js logic, need to fix views output

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Romnum} from './romnum';
+import { conversion } from './romnum';
 
 $(document).ready(function(){
-  var romnum = new Romnum(4);
-  return romnum;
+  $('#conversion-form').submit(function() {
+    var inputNum = $("#input-number").val();
+    console.log(inputNum);
+
+    $('#result').show();
+    $('#convertedNum').text(conversion(inputNum));
+    return false;
+  });
 });
--- src/romnum.js
+++ src/romnum.js
@@ -1,45 +1,69 @@
-export function Romnum(numInput) {
-  this.numInput = numInput;
+function Romnum(inputNum) {
+  this.inputNum = inputNum;
 }
 
 Romnum.prototype.checkType = function() {
-  if ((this.numInput > 0)) {
+  if ((this.inputNum > 0)) {
     return "this is an integer";
   }
-}
+};
 
 Romnum.prototype.checkV = function() {
-  if ((this.numInput === 5)) {
+  if ((this.inputNum === 5)) {
     return "V";
   }
 };
 
 Romnum.prototype.checkX = function() {
-  if ((this.numInput === 10)) {
+  if ((this.inputNum === 10)) {
     return "X";
   }
 };
 
 Romnum.prototype.checkL = function() {
-  if ((this.numInput === 50)) {
+  if ((this.inputNum === 50)) {
     return "L";
   }
 };
 
 Romnum.prototype.checkC = function() {
-  if ((this.numInput === 100)) {
+  if ((this.inputNum === 100)) {
     return "C";
   }
 };
 
 Romnum.prototype.checkD = function() {
-  if ((this.numInput === 500)) {
+  if ((this.inputNum === 500)) {
     return "D";
   }
 };
 
 Romnum.prototype.checkM = function() {
-  if ((this.numInput === 1000)) {
+  if ((this.inputNum === 1000)) {
     return "M";
   }
 };
+
+Romnum.prototype.conversion = function(inputNum) {
+  var numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1],
+      letters = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'],
+      roman = '',
+      N = inputNum,
+      i;
+
+  if (inputNum < 1) {
+    return '';
+  }
+
+  if (inputNum > 3999) {
+    return 'N';
+  }
+
+  for (i = 0; i < numbers.length; i++) {
+    while (N >= numbers[i]) {
+      roman += letters[i];
+      N -= numbers[i];
+      }
+  }
+    return roman;
+}
